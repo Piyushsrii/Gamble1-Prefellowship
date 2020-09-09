85098eed8a5a8003c49a926493b5c3b1b3c58fe4
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-09 17:00:01 +0530

    Update Gamble1.java
    [Piyush] Gamble "adding some variable variable"

--- Gamble1.java
+++ Gamble1.java
@@ -2,10 +2,15 @@
 
 public class Gamble1 
 {
-    static int stakeMoney=100;
+   static int stakeMoney=100;
     static int betMoney=1;
+ 
+    //instance variable
+    
     static int wonMoney;
     static int lossMoney;
     static int total_Won_Money=0;
     static int total_loss_Money=0;
+    static int oneDay_Won_Money=0;
+    static int oneDay_loss_Money=0; 
 }
